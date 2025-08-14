9c8d5d0a461a0e5220d29a29518e57b58b59a8d9
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-08-14 21:19:07 +0200

    Connection MongoDB

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5001;
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('Hello World !');
