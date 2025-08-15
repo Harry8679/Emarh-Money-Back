134d08472880adcbe282333a8ea6e13a6cf346f0
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-08-15 10:21:41 +0200

    Cors added

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/auth.route");
+const cors = require("cors");
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
+// ✅ Middleware CORS
+app.use(cors({
+  origin: "http://localhost:3000", // URL de ton frontend (Vite)
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization"
+}));
 app.use(express.json());
 
 app.use("/api/v1/auth", authRoutes);
