11506c8e196e3241c89edf9ed3de251a09b5d2c9
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-08-16 13:12:02 +0200

    Transaction routes

---
+++ routes/transaction.routes.js
@@ -0,0 +1,27 @@
+// routes/transaction.routes.js
+const express = require("express");
+const router = express.Router();
+
+const { protect } = require("../middleware/auth");
+const {
+  createTransaction,
+  getTransactions,
+  getTransactionById,
+  updateTransaction,
+  deleteTransaction,
+} = require("../controllers/transaction.controller");
+
+// Toutes les routes ci-dessous nécessitent un utilisateur connecté
+router.use(protect);
+
+// CRUD
+router.route("/")
+  .post(createTransaction)  // CREATE
+  .get(getTransactions);    // READ (list)
+
+router.route("/:id")
+  .get(getTransactionById)  // READ (one)
+  .put(updateTransaction)   // UPDATE
+  .delete(deleteTransaction); // DELETE
+
+module.exports = router;
