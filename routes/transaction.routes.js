3651772b1136d83a1b8164ab323f0fdb617d7ab6
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-08-23 21:37:16 +0200

    Transaction Route 4

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-// const { protect } = require("../middlewares/auth.middleware");
 const {
   createTransaction,
   getTransactions,
